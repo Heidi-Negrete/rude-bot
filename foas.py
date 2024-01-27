8ac66a3146a7a52d0acde7e5bdbf7215fcc5af15
Heidi Negrete <heidi@negrete.dev>
2024-01-27 16:11:37 -0500

    fix init method in foas to accept 1 argument

--- foas.py
+++ foas.py
@@ -3,19 +3,18 @@ import random
 
 class Foas:
     '''All the random fucks you'll never need.'''
-    fucks = ["Fuckity bye!",
-             "I can't fuckin' even.",
-             "I don't give a flying fuck.",
-             "Fuck that, fuck you.",
-             "Fuck that shit.",
-             "You disingenuous dense motherfucker!",
-             "Fuck me gently with a chainsaw.",
-             "Everyone can go and fuck off.",
-             "Kindly fuck off.",
-             ]
 
-    def __init___(self, fucks=[]):
-        self.fucks += fucks
+    def __init__(self, fucks=[]):
+        self.fucks = ["Fuckity bye!",
+                      "I can't fuckin' even.",
+                      "I don't give a flying fuck.",
+                      "Fuck that, fuck you.",
+                      "Fuck that shit.",
+                      "You disingenuous dense motherfucker!",
+                      "Fuck me gently with a chainsaw.",
+                      "Everyone can go and fuck off.",
+                      "Kindly fuck off.",
+                      ] + fucks
 
     def give_fuck(self):
         return random.choice(self.fucks)
