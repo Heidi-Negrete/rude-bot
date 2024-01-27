f4b64c12308cea7c62136592d0befb1df50e31e6
Heidi Negrete <heidi@negrete.dev>
2024-01-27 15:41:07 -0500

    revert previous revert lol

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$fuckoff'):
+    if message.content.startswith('$****off'):
         await message.channel.send('Kindly fuck off.')
 
     if 'fuck' in message.content.replace(" ", "").lower():
